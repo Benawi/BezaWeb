7265084e5a2ae29bdc126e3da7a4597860fe0f1c
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-13 22:02:27 +0300

    beza web

---
+++ script/data.js
@@ -0,0 +1,71 @@
+export default [
+  {
+    name: 'Multi-Post Stories',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: './images/culture/taba4.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '#',
+  },
+
+  {
+    name: 'Product Name: Taba',
+    description:
+      "Detail about product will be written hereDetail about product will be written hereDetail about product will be written here",
+    image: './images/culture/tabl.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '#',
+  },
+
+  {
+    name: 'Product Name: ',
+    description:
+      "Detail about product will be written hereDetail about product will be written hereDetail about product will be written here",
+    image: './images/culture/tab2.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '#',
+  },
+
+  {
+    name: 'Product Name:',
+    description:
+      "Detail about product will be written here Detail about product will be written hereDetail about product will be written here",
+    image: './images/culture/sefed.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '#',
+  },
+
+  {
+    name: 'Product Name',
+    description:
+      "Detail about product will be written here Detail about product will be written hereDetail about product will be written here",
+    image: './images/culture/jebena.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '#',
+  },
+
+  {
+    name: 'Product Name:',
+    description:
+      "Detail about product will be written hereDetail about product will be written hereDetail about product will be written here",
+    image: './images/culture/taba5.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '',
+  },
+
+  {
+    name: 'Product Name:',
+    description:
+      "Detail about product will be written hereDetail about product will be written hereDetail about product will be written here",
+    image: './images/culture/wenber.jpg',
+    technologies: [],
+    deployment: 'https://www.google.com',
+    source: '',
+  },
+];
